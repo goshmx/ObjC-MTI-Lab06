042c260d185ed8fe193f236bfa1978e41b5f4e13
Gosh <osvaldocube@gmail.com>
2015-02-06 18:55:43 -0600

    Initial Commit

---
+++ PracticaPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticaPush
+//
+//  Created by TRON on 06/02/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PracticaPush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticaPush
+//
+//  Created by TRON on 06/02/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
